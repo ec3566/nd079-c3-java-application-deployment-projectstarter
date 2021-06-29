230b84a61bcafc0d199fd38b722ac4c5967fe5a0
ec3566 <47432136+ec3566@users.noreply.github.com>
2021-06-29 05:12:13 -0400

    spotbugs

--- starter/catpoint-parent/security/src/main/java/com/udacity/catpoint/security/service/StyleService.java
+++ starter/catpoint-parent/security/src/main/java/com/udacity/catpoint/security/service/StyleService.java
@@ -7,6 +7,6 @@ import java.awt.*;
  */
 public class StyleService {
 
-	public static Font HEADING_FONT = new Font ( "Arial", Font.BOLD, 24 );
+	public final static Font HEADING_FONT = new Font ( "Arial", Font.BOLD, 24 );
 
 }
